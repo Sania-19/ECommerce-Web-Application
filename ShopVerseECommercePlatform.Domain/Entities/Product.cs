2e6e31c643ec8d61f1a0dbb8c8a9151425ce38f0
Sania Ashraf <saniaashraf1313@gmail.com>
2026-07-19 13:50:44 +0530

    Product Entity Added

---
+++ ShopVerseECommercePlatform.Domain/Entities/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShopVerseECommercePlatform.Domain.Entities
+{
+    public class Product:BaseEntity
+    {
+    }
+}
